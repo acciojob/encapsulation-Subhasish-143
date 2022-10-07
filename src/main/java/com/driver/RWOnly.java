f5f6f687dbcb98f6d2f394037665286616655f32
Subhasish Padhee <44895248+Subhasish-143@users.noreply.github.com>
2022-10-07 13:01:48 +0530

    get and set added

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,4 +2,10 @@ package com.driver;
 
 public class RWOnly {
     private String name;
+    public void setName(String str) {
+        name = str;
+    }
+    public String getName() {
+        return name;
+    }
 }
