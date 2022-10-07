2280e3e190004f7ce52e6e7e080bc72ef5c0eace
Subhasish Padhee <44895248+Subhasish-143@users.noreply.github.com>
2022-10-07 13:07:49 +0530

    encapsulation done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
+import java.util.*;
+
 public class Main {
-  
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        RWOnly rw = new RWOnly();
+        
+        // wrong step 1st-- 
+        //rw.name = str;
+        // Error:- name has private access in Encapsulate
+        
+        rw.setName(str);
+        System.out.println(getName());
+    }
 }
